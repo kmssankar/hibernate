769b755110ce6604ba5ada7b03adc9692154ceda
kmssankar <s.sankarganesh@hotmail.com>
2019-10-15 07:25:50 +0530

    hibernate Initial Commit

---
+++ hibernate/src/main/java/org/kms/entities/Vehicle.java
@@ -0,0 +1,68 @@
+package org.kms.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Vehicle {
+	@Id
+	@Column(name="vin")
+	String vin;
+	@Column(name="mdlcd")
+	int mdlcd;
+	@Column(name="mdlyr")
+	int mdlyr;
+	@Column(name="mdldesc")
+	String mdldesc;
+	/**
+	 * @return the vin
+	 */
+	public String getVin() {
+		return vin;
+	}
+	/**
+	 * @param vin the vin to set
+	 */
+	public void setVin(String vin) {
+		this.vin = vin;
+	}
+	/**
+	 * @return the mdlcd
+	 */
+	public int getMdlcd() {
+		return mdlcd;
+	}
+	/**
+	 * @param mdlcd the mdlcd to set
+	 */
+	public void setMdlcd(int mdlcd) {
+		this.mdlcd = mdlcd;
+	}
+	/**
+	 * @return the mdlyr
+	 */
+	public int getMdlyr() {
+		return mdlyr;
+	}
+	/**
+	 * @param mdlyr the mdlyr to set
+	 */
+	public void setMdlyr(int mdlyr) {
+		this.mdlyr = mdlyr;
+	}
+	/**
+	 * @return the mdldesc
+	 */
+	public String getMdldesc() {
+		return mdldesc;
+	}
+	/**
+	 * @param mdldesc the mdldesc to set
+	 */
+	public void setMdldesc(String mdldesc) {
+		this.mdldesc = mdldesc;
+	}
+
+	
+}
